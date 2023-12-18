b7ce4edcdce95b951a4154e73602685f8de705e9
Melody <melody.mackey@gmail.com>
2023-12-17 22:13:51 -0600

    Day 1 part one completed. Make sure End of Line Sequence is changed to LF in the txt files to work

--- day-1/index.js
+++ day-1/index.js
@@ -1,5 +1,29 @@
-import fs from 'fs';
+ import fs from 'fs';
 
-const lines = fs.readFileSync('./example.txt', 'utf-8').trim().split('\n');
+const lines = fs.readFileSync('./input.txt', 'utf-8').trim().split('\n');
 
-console.log(lines)
+function partOne(){
+   const values = lines
+   .map((line) => {
+    let first = line.split('').find((v) => Number.isInteger(Number(v)));
+    let last = line.split('').findLast((v) => Number.isInteger(Number(v)));
+    return Number(first + last);
+   });
+
+   return values.reduce((s, v) => s + v);
+}
+
+console.log(partOne('./input.txt'));
+
+
+// function partOne(){
+//     const values = lines
+//     .forEach((line) => {
+//      let first = line.split('').find((v) => !Number.isNaN(Number(v)));
+//      let last = line.split('').findLast((v) => !Number.isNaN(Number(v)));
+//      return Number(first + last);
+//     });
+ 
+//     console.log(values);
+//  }
+
